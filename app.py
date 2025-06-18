f105ebced5b73f7a52f6a9067e24b86774e0fa45
Naneagnes <agnessalenda@students.amikom.ac.id>
2025-06-18 13:13:50 +0700

    app.py

--- app.py
+++ app.py
@@ -2,23 +2,24 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-# Load model
-model = joblib.load('model_kualitas_air.pkl')  # Ganti dengan nama file model kamu
+# Load model dan scaler
+model = joblib.load('model_rf.pkl')
+scaler = joblib.load('scaler.pkl')
 
 # UI
-st.title("Prediksi Kualitas Air Budidaya Ikan")
-st.markdown("Masukkan parameter air untuk mengetahui kualitasnya.")
+st.title("Prediksi Kualitas Air")
+st.markdown("Masukkan parameter air untuk memprediksi kualitasnya.")
 
 # Input
-bod = st.number_input('BOD (mg/L)', min_value=0.0)
-do = st.number_input('DO (mg/L)', min_value=0.0)
-turbidity = st.number_input('Turbidity (cm)', min_value=0.0)
-h2s = st.number_input('H2S (mg L-1)', min_value=0.0)
-nitrite = st.number_input('Nitrite (mg L-1)', min_value=0.0)
+bod = st.number_input("BOD (mg/L)", min_value=0.0)
+do = st.number_input("DO (mg/L)", min_value=0.0)
+turb = st.number_input("Turbidity (cm)", min_value=0.0)
+h2s = st.number_input("H2S (mg/L)", min_value=0.0)
+nitrite = st.number_input("Nitrite (mg/L)", min_value=0.0)
 
 if st.button("Prediksi"):
-    data = pd.DataFrame([[bod, do, turbidity, h2s, nitrite]],
-                        columns=['BOD (mg/L)', 'DO(mg/L)', 'Turbidity (cm)', 'H2S (mg L-1 )', 'Nitrite (mg L-1 )'])
-    pred = model.predict(data)[0]
+    data = [[bod, do, turb, h2s, nitrite]]
+    data_scaled = scaler.transform(data)
+    pred = model.predict(data_scaled)[0]
     label = {0: "Buruk", 1: "Sedang", 2: "Baik"}
     st.success(f"Kualitas Air: {label[pred]}")
